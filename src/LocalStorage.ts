d17a3969af6178f490fc88b4c1fae6751f7ced41
alssel2525 <alssel2525@naver.com>
2022-10-26 11:16:51 +0900

    Refactor LocalStorage to typescript

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,24 +1,25 @@
 class LocalStorage {
 	static instance = null;
-	
+	storage: Storage;
+	gameStateKey: string;
+
 	constructor() {
 		if (LocalStorage.instance) {
 			return LocalStorage.instance;
 		}
-		
+
 		this.storage = window.localStorage;
 		this.gameStateKey = "gameState";
-		
+
 		LocalStorage.instance = this;
 	}
-	
+
 	setGameState(gameState) {
 		this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
 	}
-	
+
 	getGameState() {
 		let gameState = this.storage.getItem(this.gameStateKey);
-		console.log(gameState);
 		try {
 			return gameState ? JSON.parse(gameState) : null;
 		} catch (e) {
